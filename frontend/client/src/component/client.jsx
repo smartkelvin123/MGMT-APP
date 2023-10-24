f9aa96044277f680cbf88795b83f646f46f78350
unknown <smartkelvin66@gmail.com>
2023-10-24 14:48:13 +0100

    updated tables

--- frontend/client/src/component/client.jsx
+++ frontend/client/src/component/client.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+
+import ClientRow from "./clientRow";
 import { gql, useQuery } from "@apollo/client";
 
-// fetch data from backend
+// Fetch data from the backend
 const GET_CLIENTS = gql`
   {
     clients {
@@ -18,39 +20,24 @@ const Clients = () => {
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(something is wrong)</p>;
-  console.log("data", data);
 
   return (
     <div>
-      {!loading && !error && (
-        <table className="table table-hover">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Phone</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.clients.map((client) => (
-              <tr key={client.id}>
-                <td>{client.name}</td>
-                <td>{client.email}</td>
-                <td>{client.phone}</td>
-              </tr>
-            ))}
-          </tbody>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Phone</th>
-              <th></th>
-            </tr>
-          </thead>
-        </table>
-      )}
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.clients.map((client) => (
+            <ClientRow key={client.id} client={client} />
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
