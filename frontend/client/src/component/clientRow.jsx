131ab019070ca158ca1e7765d47fc4a3f3725b77
unknown <smartkelvin66@gmail.com>
2023-10-24 15:29:37 +0100

    updated

--- frontend/client/src/component/clientRow.jsx
+++ frontend/client/src/component/clientRow.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import { FaTrash } from "react-icons/fa";
+import { DELETE_CLIENT } from "../mutation/clientMuataion";
+import { useMutation } from "@apollo/client";
+import { GET_CLIENTS } from "../queries/clientQueries";
+
+// note : rememeber that apolo is a state manaber. and also no prop drilling
 
 const ClientRow = ({ client }) => {
+  const [deleteClient] = useMutation(DELETE_CLIENT, {
+    variables: {
+      id: client.id,
+    },
+    refetchQueries: [{ query: GET_CLIENTS }],
+  });
   return (
     <tr>
       <td>{client.name}</td>
       <td>{client.email}</td>
       <td>{client.phone}</td>
       <td>
-        <button className="btn btn-danger btn-sm">
+        <button className="btn btn-danger btn-sm" onClick={deleteClient}>
           <FaTrash />
         </button>
       </td>
