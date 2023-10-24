e5e57df370071a7863faf610b7217e865e11fab7
unknown <smartkelvin66@gmail.com>
2023-10-24 14:36:06 +0100

    tables

--- frontend/client/src/component/client.jsx
+++ frontend/client/src/component/client.jsx
@@ -15,11 +15,44 @@ const GET_CLIENTS = gql`
 
 const Clients = () => {
   const { loading, error, data } = useQuery(GET_CLIENTS);
+
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(something is wrong)</p>;
   console.log("data", data);
 
-  return <div>{!loading && !error && <h1>Clients</h1>}</div>;
+  return (
+    <div>
+      {!loading && !error && (
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.clients.map((client) => (
+              <tr key={client.id}>
+                <td>{client.name}</td>
+                <td>{client.email}</td>
+                <td>{client.phone}</td>
+              </tr>
+            ))}
+          </tbody>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th></th>
+            </tr>
+          </thead>
+        </table>
+      )}
+    </div>
+  );
 };
 
 export default Clients;
