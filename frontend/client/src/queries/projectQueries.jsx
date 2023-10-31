cda61e129ea4f41430b0de9bd61a012a8e09b7ad
unknown <smartkelvin66@gmail.com>
2023-10-31 12:59:01 +0100

    get_project

--- frontend/client/src/queries/projectQueries.jsx
+++ frontend/client/src/queries/projectQueries.jsx
@@ -10,4 +10,22 @@ const GET_PROJECTS = gql`
   }
 `;
 
-export { GET_PROJECTS };
+// get single project
+const GET_PROJECT = gql`
+  query getProject($id: ID!) {
+    project(id: $id) {
+      id
+      name
+      status
+      description
+      client {
+        id
+        name
+        email
+        phone
+      }
+    }
+  }
+`;
+
+export { GET_PROJECTS, GET_PROJECT };
