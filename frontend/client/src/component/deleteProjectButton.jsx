d48f0e218cc551c06361df1303cf4cb05a957c4f
unknown <smartkelvin66@gmail.com>
2023-11-01 13:58:06 +0100

    updated deleteButton

--- frontend/client/src/component/deleteProjectButton.jsx
+++ frontend/client/src/component/deleteProjectButton.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { FaTrash } from "react-icons/fa";
+import { GET_PROJECT } from "../queries/projectQueries";
+import { useMutation } from "@apollo/client";
+import { DELETE_PROJECT } from "../mutation/projectMutation";
 
-const DeleteProjectButton = () => {
-  return <div>DeleteProjectButton</div>;
+const DeleteProjectButton = ({ projectId }) => {
+  const navigate = useNavigate();
+  //   const { loading, error, data } = useQuery(GET_PROJECT);
+  const [deleteProject] = useMutation(DELETE_PROJECT, {
+    variables: {
+      id: projectId,
+    },
+
+    onCompleted: () => navigate("/"),
+    refetchQueries: [{ query: GET_PROJECT }],
+  });
+
+  return (
+    <div className="d-flex mt-5 ms-auto">
+      <button className="btn btn-danger m-2">
+        <FaTrash
+          className="icon"
+          onClick={() => {
+            deleteProject();
+          }}
+        />
+        Delete project
+      </button>
+    </div>
+  );
 };
 
 export default DeleteProjectButton;
