bbbae90c786710e0d8f9a412b426a78221267aa6
unknown <smartkelvin66@gmail.com>
2023-11-13 15:56:40 +0100

    clean

--- frontend/client/src/pages/login.jsx
+++ frontend/client/src/pages/login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-// import { useAuth } from "./AuthContext";
+
 import { useAuth } from "../component/AuthContext";
 
 import { useMutation } from "@apollo/client";
